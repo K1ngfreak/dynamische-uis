d060793d964fc596dbaf33cca523c83cf4e0351a
K1ngfreak <noahzonneveld@hotmail.com>
2023-02-23 12:12:24 +0100

    de knoppen werken

---
+++ buttons-template/buttons 1.0/buttons.js
@@ -0,0 +1,15 @@
+button1 = 0
+button2 = 0
+button3 = 0
+
+function buttonFunction(buttonNumber) {
+    if (buttonNumber == 0) {
+        document.getElementById("imgid").src="images/0.jpg";
+    } else if (buttonNumber == 1) {
+        document.getElementById("imgid").src="images/1.jpg";
+    } else if (buttonNumber == 2) {
+        document.getElementById("imgid").src="images/2.jpg";
+    } else if (buttonNumber == 3) {
+        document.getElementById("imgid").src="images/3.jpg";
+    }
+}
