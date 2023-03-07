3d27806c6c3e332d720b1730d891bd3bf187ab4c
K1ngfreak <noahzonneveld@hotmail.com>
2023-03-07 11:30:34 +0100

    the tracks rotate when you press a key that makes the tank move

--- tank/tank.js
+++ tank/tank.js
@@ -3,6 +3,8 @@ var image = document.getElementById("image");
 document.onkeydown = checkKey;
 image.style.transform = "rotate(90deg)"
 
+var position = 80;
+
 function checkKey(e) {
 	console.log("key nr = " + e.keyCode);
     e = e || window.event;
@@ -14,11 +16,18 @@ function checkKey(e) {
         console.log("down");
     } else if (e.keyCode == '37' || e.keyCode == '65') { // left arrow or a
     	console.log("left");
-        image.style.left = image.offsetLeft + 20 + "px";
     } else if (e.keyCode == '39' || e.keyCode == '68') {   // right arrow or d
     	console.log("right");
-    	image.style.backgroundPosition = `164px 0px`; // check goed de rupsband
+    	// image.style.backgroundPosition = `164px 0px`; // check goed de rupsband
     }
+    if (e.keyCode == 38 || e.keyCode == 87 || e.keyCode == 40 || e.keyCode == 83 || e.keyCode == 37 || e.keyCode == 65 || e.keyCode == 39 || e.keyCode == 68) {
+        document.getElementById("image").style.backgroundPosition = 
+        `-${position}px 0px`; 
+        if (position < 668)
+        { position = position + 84;}
+        else
+        { position = 80; }
+    }
+   
 }
 
-// 80, 164, 248, 332, 416, 500, 584, 668
