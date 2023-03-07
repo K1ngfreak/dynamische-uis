17860f75f1c2044043a66cf036b61cf5dadd16f7
K1ngfreak <noahzonneveld@hotmail.com>
2023-03-07 10:12:02 +0100

    made controlling with WASD possible

--- tank/tank.js
+++ tank/tank.js
@@ -8,14 +8,14 @@ function checkKey(e) {
     e = e || window.event;
     if(e.keyCode == 32){
     	console.log("spacebar");
-    } else if (e.keyCode == '38') {  // up arrow
-        console.log("Up arrow");
-    } else if (e.keyCode == '40') { // down arrow
-        console.log("down arrow");
-    } else if (e.keyCode == '37') { // left arrow
-    	console.log("left arrow");
-    } else if (e.keyCode == '39') {   // right arrow
-    	console.log("right arrow");
+    } else if (e.keyCode == '38' || e.keyCode == '87') {  // up arrow or w
+        console.log("Up");
+    } else if (e.keyCode == '40' || e.keyCode == '83') { // down arrow or s
+        console.log("down");
+    } else if (e.keyCode == '37' || e.keyCode == '65') { // left arrow or a
+    	console.log("left");
+    } else if (e.keyCode == '39' || e.keyCode == '68') {   // right arrow or d
+    	console.log("right");
     	image.style.backgroundPosition = `164px 0px`; // check goed de rupsband
     }
 }
