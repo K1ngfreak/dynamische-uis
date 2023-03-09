7ab66726288e8337e40fbc098097e01aafe65a03
K1ngfreak <noahzonneveld@hotmail.com>
2023-03-09 17:20:47 +0100

    cleaned up the page

--- buttons-template/buttons 3.0/buttons.js
+++ buttons-template/buttons 3.0/buttons.js
@@ -13,34 +13,27 @@ function buttonChange(button) {
         }
         list[button] += 1;
     }
-    console.log(list)
 }
 
 const list = []
-
 totalButton = Number(prompt("how many buttons do you want?"))
+body = document.getElementsByTagName("body")[0];
 
 br = 5
-
 if (totalButton > 50) {
     br = Math.ceil(totalButton/10)
 }
 
 x = 0
-
-let body = document.getElementsByTagName("body")[0];
-
 for (i = 1; i <= totalButton; i++) {
+    x++
     button = document.createElement("button");
-    button.innerHTML = "Button "+i;
-    button.id = "Button "+i
+    button.innerHTML = "Button " + i;
+    button.id = "Button " + i
     body.appendChild(button);
     button.addEventListener ("click", function() {
     buttonChange(this.innerHTML);
-    });
-
-    x++
-
+    })
     if (x == br) {
         x = 0
         document.write("<br>")
