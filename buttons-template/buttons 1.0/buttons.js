6a4bd8b0ed1a111ea89d5f768c77b73ebcc0954f
K1ngfreak <noahzonneveld@hotmail.com>
2023-03-16 15:44:37 +0100

    finished updating buttons 1.0

--- buttons-template/buttons 1.0/buttons.js
+++ buttons-template/buttons 1.0/buttons.js
@@ -1,3 +1,6 @@
-function buttonFunction(buttonNumber) {    
-    document.getElementById("imgid").src="images/"+buttonNumber+".jpg"
+function buttonFunction(button) {    
+    document.getElementById("imgId").src="images/"+button.id+".jpg"
+    btnName = parseInt(button.innerHTML)
+    button.innerHTML = btnName + 1
+    document.getElementById("bgImgId").style.backgroundImage = "url('images/bg"+button.id+".jpg')"
 }
