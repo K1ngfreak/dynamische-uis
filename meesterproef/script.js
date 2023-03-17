30102c9451e4246164458455e07f4b2912048e68
K1ngfreak <noahzonneveld@hotmail.com>
2023-03-17 13:33:21 +0100

    the board is set up and you can choose a color

--- meesterproef/script.js
+++ meesterproef/script.js
@@ -1,5 +1,73 @@
+function startGame() {
+    document.getElementById("start").innerHTML = "start a new game"
+    document.getElementById("start").style.right = "260px"
+
+    if (y > 0) {
+        for (i=1; i <= 52; i++) {
+            document.getElementById(i).remove()
+        }
+        for (o=1; o <= 4; o++) {
+            document.getElementById(o).remove()
+        }
+    }
+    x = 0
+    for (i=52; i > 4; i--) {
+        x++
+        placement = document.getElementsByTagName("div")[0];
+        button = document.createElement("button");
+        button.innerHTML = "";
+        button.style.padding = "13px"
+        button.style.height = "20px"
+        button.style.width = "20px"
+        button.id = i
+        button.disabled = true
+        placement.appendChild(button);
+        button.addEventListener ("click", function() {
+            colorChange(this);
+        })
+        if (x == 4) {
+            para = document.createElement("p")
+            div.appendChild(para)
+            x = 0
+        }
+        if (i == 5) {
+            for (u=4; u > 0; u--) {
+            button = document.createElement("button");
+            button.innerHTML = "";
+            button.style.padding = "13px"
+            button.style.height = "20px"
+            button.style.width = "20px"
+            button.id = u
+            placement.appendChild(button);
+            button.addEventListener ("click", function() {
+                colorChange(this);
+            })
+            }
+        }
+    }
+    y += 1
+}
+
+y = 0
+
+function colorChange(button) {
+    console.log(button)
+    if (document.getElementById(button.id).style.backgroundColor == "" || document.getElementById(button.id).style.backgroundColor == "red"){
+        document.getElementById(button.id).style.backgroundColor = "white"
+    } else {
+        for (i=0; i < colors.length; i++) {
+            if (colors[i] == button.style.backgroundColor) {
+                button.style.backgroundColor=colors[i+1]
+                break
+            }
+        }
+    }
+}
+
 const colors = ["white", "blue", "orange", "yellow", "green", "red"]
 const code = []
+const guess = []
+
 // random number code generator
 for (i=0; i < 4; i++) {
     randomColor = Math.floor(Math.random()*colors.length)
@@ -7,3 +75,12 @@ for (i=0; i < 4; i++) {
 }
 
 console.log(code)
+
+placement1 = document.getElementsByTagName("div")[0];
+button = document.createElement("button");
+button.innerHTML = "Start a game";
+button.id = "start"
+placement1.appendChild(button);
+button.addEventListener ("click", function() {
+    startGame();
+})
