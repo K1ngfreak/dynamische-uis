92ee107bd6eb941f2fd4d5f74af43473a2f148f5
K1ngfreak <noahzonneveld@hotmail.com>
2023-03-17 09:42:32 +0100

    button 1.0 geupdate

--- buttons-template/buttons 1.0/buttons.js
+++ buttons-template/buttons 1.0/buttons.js
@@ -2,5 +2,5 @@ function buttonFunction(button) {
     document.getElementById("imgId").src="images/"+button.id+".jpg"
     btnName = parseInt(button.innerHTML)
     button.innerHTML = btnName + 1
-    document.getElementById("bgImgId").style.backgroundImage = "url('images/bg"+button.id+".jpg')"
+    document.getElementById("container").style.backgroundImage = "url('images/bg"+button.id+".jpg')"
 }
